153250a4da29e50eda2cc4c8688272715c52e122
opmind <raitue@ya.ru>
2018-01-07 02:54:16 +0300

    add modx install task

---
+++ install_gitify.sh
@@ -0,0 +1,34 @@
+# Let's install composer:
+#cd /tmp/
+#curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer
+
+# Go to home folder and donwload + install Gitify
+# cd ~
+# git clone https://github.com/modmore/Gitify.git Gitify
+# cd Gitify
+
+# Make sure gitify is globally available
+# echo "export PATH=/home/vagrant/Gitify/:$PATH" >> ~/.profile
+# export PATH=/home/vagrant/Gitify/:$PATH
+
+echo "export PATH=/vagrant/Gitify/:$PATH" >> ~/.profile
+export PATH=/vagrant/Gitify/:$PATH
+
+# Refresh profile
+# source ~/.profile
+
+# install dependencies and make executable
+# composer install
+# sudo chmod +X Gitify
+
+# Now let's set up gitify in the www-folder:
+# and install the latest modx:
+cd /vagrant/modx
+. ~/.profile
+Gitify modx:install latest -vvv
+
+# Install all the packages that are specified in the .gitify-file in project/.gitify
+## Gitify install:package --all
+
+# Extract all the settings & files specified in the .gitify-file in project/.gitify
+## Gitify build
